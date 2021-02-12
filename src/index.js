d9feecf21e54af01c8573f63f20ec58d862e08e7
krkmariia <krykunmariia@gmail.com>
2021-02-12 15:56:49 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const landscapeImg = React.createElement(
+    "img",
+    {
+        src: "https://images.unsplash.com/photo-1612815292139-3305b3c5dd3a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+        alt: "landscape"
+    },
+)
+
+const headline = React.createElement(
+    "h1",
+    {},
+    "To travel is to live",
+)
+
+const box = React.createElement(
+    "div",
+    {
+        children: [landscapeImg, headline],
+    },
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(box, document.getElementById("root"));
